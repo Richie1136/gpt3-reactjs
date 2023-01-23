7ae3a5caae1a2d4871c4c2ea9ba8812ad415377d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-23 11:47:39 -0500

    Adding styling to the NavBar component

--- gpt3/src/components/navbar/NavBar.js
+++ gpt3/src/components/navbar/NavBar.js
@@ -1,8 +1,16 @@
 import './NavBar.css'
+import { RiMenu3LIne, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='gpt3-navbar'>
+      <div className='gpt3-navbar-links'>
+        <div className='gpt3-navbar-links-logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
