44bb3d63a7f863b19a43a5be1af7e16741f3aa03
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-23 10:53:38 -0500

    Imported all of the container files and the component files inside of App.js and then am using them in the file

--- gpt3/src/App.js
+++ gpt3/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { Footer, Blog, Possibility, Features, WhatGpt3, Header } from './containers';
+import { CallToAction, Brand, NavBar } from './components';
 
 const App = () => (
   <div className="App">
-    <h2>GPT3</h2>
+    <div className='gradient-bg'>
+      <NavBar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGpt3 />
+    <Features />
+    <Possibility />
+    <CallToAction />
+    <Blog />
+    <Footer />
   </div>
 );
 
---
+++ gpt3/src/containers/index.js
@@ -0,0 +1,8 @@
+import Blog from "./blog/Blog";
+import Features from "./features/Features";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import Possibility from "./possibility/Possibility";
+import WhatGpt3 from "./whatgpt3/WhatGpt3";
+
+export { Blog, Features, Footer, Header, Possibility, WhatGpt3 }
