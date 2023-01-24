63bd9e9a968104c1b7a2a4e5aa9300a6c959b2e6
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-24 09:13:14 -0500

    Added styling to my classes inside of the NavBar component

--- gpt3/src/components/navbar/NavBar.js
+++ gpt3/src/components/navbar/NavBar.js
@@ -9,6 +9,13 @@ const NavBar = () => {
         <div className='gpt3-navbar-links-logo'>
           <img src={logo} alt='logo' />
         </div>
+        <div className='gpt3-navbar-links-container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
       </div>
     </div>
   )
