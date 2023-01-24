aaf00508545a3fe6d8a1e96c4c5da95edd653db7
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-24 13:33:31 -0500

    Starting to build out the Brand component

--- gpt3/src/components/brand/Brand.js
+++ gpt3/src/components/brand/Brand.js
@@ -1,8 +1,11 @@
 import './Brand.css'
+import { google, slack, shopify, dropbox, atlassian } from './imports'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3-brand section-padding'>
+
+    </div>
   )
 }
 
