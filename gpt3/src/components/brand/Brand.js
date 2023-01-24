b08781ecab9473545c634dd86f11f667d8594741
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-24 13:39:33 -0500

    Finished the brand component

--- gpt3/src/components/brand/Brand.js
+++ gpt3/src/components/brand/Brand.js
@@ -4,7 +4,21 @@ import { google, slack, shopify, dropbox, atlassian } from './imports'
 const Brand = () => {
   return (
     <div className='gpt3-brand section-padding'>
-
+      <div>
+        <img src={google} alt='Google' />
+      </div>
+      <div>
+        <img src={slack} alt='Slack' />
+      </div>
+      <div>
+        <img src={atlassian} alt='Atlassian' />
+      </div>
+      <div>
+        <img src={dropbox} alt='Dropbox' />
+      </div>
+      <div>
+        <img src={shopify} alt='Shopify' />
+      </div>
     </div>
   )
 }
