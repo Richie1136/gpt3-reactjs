54c48f97eedb68b53a4fbd7bafb7d76f5bc6b9be
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-25 09:56:38 -0500

    Adding styling to the feature component

--- gpt3/src/components/feature/Feature.js
+++ gpt3/src/components/feature/Feature.js
@@ -1,17 +1,15 @@
 import './Feature.css'
 
-const Feature = ({ title, text }) => {
-  return (
-    <div className='gpt3-features-container-feature'>
-      <div className='gpt3-features-container-feature-title'>
-        <div></div>
-        <h1>{title}</h1>
-        <div className='gpt3-features-container-feature-text'>
-          {text}
-        </div>
-      </div>
+const Feature = ({ title, text }) => (
+  <div className='gpt3-features-container-feature'>
+    <div className='gpt3-features-container-feature-title'>
+      <div />
+      <h1>{title}</h1>
     </div>
-  )
-}
+    <div className='gpt3-features-container-feature-text'>
+      <p>{text}</p>
+    </div>
+  </div>
+)
 
 export default Feature
