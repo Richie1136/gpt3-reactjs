7f6a1ffc64e2358fa95b447f0b52882457cae46c
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-25 13:13:39 -0500

    Building out the blog component Structure

--- gpt3/src/containers/blog/Blog.js
+++ gpt3/src/containers/blog/Blog.js
@@ -2,7 +2,19 @@ import './Blog.css'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3-blog section-padding' id='blog'>
+      <div className='gpt3-blog-heading'>
+        <h1 className='gradient-text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3-blog-container'>
+        <div className='gpt3-blog-container-groupA'>
+
+        </div>
+        <div className='gpt3-blog-container-groupB'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
