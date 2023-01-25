3b90fb26563397894cfab13337c586359dc161ab
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-25 11:53:48 -0500

    Coded out the click to action component and added the proper styling

--- gpt3/src/components/clicktoaction/ClickToAction.js
+++ gpt3/src/components/clicktoaction/ClickToAction.js
@@ -2,7 +2,15 @@ import './ClickToAction.css'
 
 const ClickToAction = () => {
   return (
-    <div>CallToAction</div>
+    <div className='gpt3-cta'>
+      <div className='gpt3-cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possibilities</h3>
+      </div>
+      <div className='gpt3-cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
