88f57fc14b9852d713547fe7c24cd26dfc84ac0e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2023-01-25 10:48:31 -0500

    Fixed route issue

--- gpt3/src/containers/whatgpt3/WhatGpt3.js
+++ gpt3/src/containers/whatgpt3/WhatGpt3.js
@@ -3,7 +3,7 @@ import Feature from '../../components/feature/Feature'
 
 const WhatGpt3 = () => {
   return (
-    <div className='gpt3-whatgpt3 section-margin' id='whpt3'>
+    <div className='gpt3-whatgpt3 section-margin' id='wgpt3'>
       <div className='gpt3-whatgpt3-feature'>
         <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
       </div>
